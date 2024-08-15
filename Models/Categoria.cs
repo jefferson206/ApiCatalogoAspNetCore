e0838f674fef23368621f38e90aed4d108a0788e
Jefferson Mendonca <jefferson.mendonca@theos.com.br>
2024-08-15 19:10:52 -0300

    Adicionar arquivos de projeto.

---
+++ Models/Categoria.cs
@@ -0,0 +1,9 @@
+﻿namespace ApiCatalogo.Models;
+
+
+public class Categoria
+{
+    public int CategoriaId { get; set; }
+    public string? Nome { get; set; }
+    public string? ImagemUrl { get; set; }
+}
