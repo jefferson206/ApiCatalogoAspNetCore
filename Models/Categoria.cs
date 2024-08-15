2dd6381fdb59a923c28806c0d9014f194181adca
Jefferson Mendonca <jefferson.mendonca@theos.com.br>
2024-08-15 20:07:29 -0300

    Implementação do DB Context, configuração da string connection na classe program, e alteração dos relacionamentos entre as tabelas de protudo e categoria

--- Models/Categoria.cs
+++ Models/Categoria.cs
@@ -1,9 +1,17 @@
-﻿namespace ApiCatalogo.Models;
+﻿using System.Collections.ObjectModel;
+
+namespace ApiCatalogo.Models;
 
 
 public class Categoria
 {
+    public Categoria()
+    {
+        Produtos = new Collection<Produto>();
+    }
+
     public int CategoriaId { get; set; }
     public string? Nome { get; set; }
     public string? ImagemUrl { get; set; }
+    public ICollection<Produto>? Produtos { get; set; }
 }
