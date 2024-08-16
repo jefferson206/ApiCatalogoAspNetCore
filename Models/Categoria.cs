cdbccd251d61ae6a7369caf89ce1f1753773416b
Mendonca, Jefferson <jefferson.mendonca_contractor@celanese.com>
2024-08-16 18:20:43 -0300

    Implementação de data annotation nas classes para modificar as tabelas no migrations.

--- Models/Categoria.cs
+++ Models/Categoria.cs
@@ -1,8 +1,10 @@
 ﻿using System.Collections.ObjectModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace ApiCatalogo.Models;
 
-
+[Table("Categorias")]
 public class Categoria
 {
     public Categoria()
@@ -10,8 +12,15 @@ public class Categoria
         Produtos = new Collection<Produto>();
     }
 
+    [Key]
     public int CategoriaId { get; set; }
+    
+    [Required]
+    [StringLength(80)]
     public string? Nome { get; set; }
+
+    [Required]
+    [StringLength(300)]
     public string? ImagemUrl { get; set; }
     public ICollection<Produto>? Produtos { get; set; }
 }
