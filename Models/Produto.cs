5bd3da8d3f5ee2e3042cf7b359b9c72516aedaa9
Jefferson Mendonca <jefferson.mendonca@theos.com.br>
2024-08-27 18:34:15 -0300

    Implementação de custom validations, dataAnnotation validations e IvalidatableObject

--- Models/Produto.cs
+++ Models/Produto.cs
@@ -1,4 +1,5 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using ApiCatalogo.Validations;
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Text.Json.Serialization;
 
@@ -6,17 +7,19 @@ namespace ApiCatalogo.Models;
 
 [Table("Produtos")]
 
-public class Produto
+public class Produto : IValidatableObject
 {
     [Key]
     public int ProdutoId { get; set; }
 
-    [Required]
-    [StringLength(80)]
+    [Required(ErrorMessage = "O nome é obrigatório")]
+    [StringLength(80, ErrorMessage = "O nome deve ter no máximo {1} e no mínimo {2} caracteres", MinimumLength = 5)]
     public string? Nome { get; set; }
     
     [Required]
-    [StringLength(300)]
+    [StringLength(300, ErrorMessage = "A descrição deve ter no máximo {1} caracteres")]
+    [PrimeiraLetraMaiuscula]
+
     public string? Descricao { get; set; }
     
     [Required]
@@ -33,4 +36,21 @@ public class Produto
     [JsonIgnore]
     public Categoria? Categoria { get; set; }
 
+    public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+    {
+
+        if (!string.IsNullOrEmpty(this.Nome))
+        {
+            var primeiraLetra = this.Nome[0].ToString();
+            if (primeiraLetra != primeiraLetra.ToUpper())
+            {
+                yield return new ValidationResult("A primeira letra do produto deve ser maiúscula.", new[] { nameof(this.Nome) });
+            }
+        }
+
+        if (this.Preco <= 0)
+        {
+            yield return new ValidationResult("O Preço deve ser maior que zero", new[] { nameof(this.Preco) });
+        }
+    }
 }
